3409c8dbec07a18aeab2cdc8b265658696f46cac
Mesakh Lolo <91011194+mesakhlolo@users.noreply.github.com>
2025-08-16 22:42:23 +0700

    ambil data dari input dan masukkan ke html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log("Hello World!");
+const addTaskForm = document.querySelector(".add-task-form");
+const taskInput = addTaskForm.querySelector("#add-task-input");
+const taskList = document.querySelector(".task-list");
+
+addTaskForm.addEventListener("submit", function (event) {
+  // biar tidak reload
+  event.preventDefault();
+
+  // bersihkan input
+  const task = taskInput.value.trim();
+  if (!task) return;
+
+  // pakai data
+  const taskItem = document.createElement("li");
+  taskItem.classList.add("task-item");
+  taskItem.innerHTML = `
+    <div class="task-main">
+        <input type="checkbox" id="task1" label="task1" />
+        <label for="task1">${task}</label>
+    </div>
+    <div class="task-actions">
+        <button type="button" class="edit-btn">Edit</button>
+        <button type="button" class="delete-btn">Delete</button>
+    </div>
+`;
+
+  // tambahkan taskItem <li> ke dalam taskList <ul>
+  taskList.appendChild(taskItem);
+
+  // reset form dan focus input
+  addTaskForm.reset();
+  taskInput.focus();
+});
