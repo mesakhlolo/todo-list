d164dd38a5c406d1df4db66c93cd280b3514e3bc
Mesakh Lolo <91011194+mesakhlolo@users.noreply.github.com>
2025-08-17 21:39:22 +0700

    buat factory function todo, ambil data, pakai data dengan inner html

--- src/index.js
+++ src/index.js
@@ -1,27 +1,54 @@
 const addTaskForm = document.querySelector(".add-task-form");
-const taskInput = addTaskForm.querySelector("#add-task-input");
+const taskInput = addTaskForm.querySelector("#add-task-title");
 const taskList = document.querySelector(".task-list");
+const descriptionInput = document.querySelector("#add-task-description");
+const dateInput = document.querySelector("#add-task-date");
+
+function createTodo(title, description, dueDate, priority) {
+  return {
+    title,
+    description,
+    dueDate,
+    priority,
+  };
+}
 
 addTaskForm.addEventListener("submit", function (event) {
   // biar tidak reload
   event.preventDefault();
 
-  // bersihkan input
-  const task = taskInput.value.trim();
-  if (!task) return;
+  // ambil data
+  const title = taskInput.value.trim();
+  const description = descriptionInput.value.trim();
+  const dueDate = dateInput.value;
+  const priority = document.querySelector(
+    "input[name='priority']:checked"
+  )?.value;
+
+  // cek input kosong
+  if (!title || !priority) {
+    alert("Task dan Priority harus diisi!");
+    return;
+  }
+
+  // buat object todo
+  const todo = createTodo(title, description, dueDate, priority);
 
   // pakai data
   const taskItem = document.createElement("li");
   taskItem.classList.add("task-item");
   taskItem.innerHTML = `
-    <div class="task-main">
-        <input type="checkbox" id="task1" label="task1" />
-        <label for="task1">${task}</label>
-    </div>
-    <div class="task-actions">
-        <button type="button" class="edit-btn">Edit</button>
-        <button type="button" class="delete-btn">Delete</button>
-    </div>
+  <div class="task-main">
+    <input type="checkbox" />
+    <label>${todo.title}</label>
+    <p>${todo.description}</p>
+    <p>Due: ${todo.dueDate}</p>
+    <p>Priority: ${todo.priority}</p>
+  </div>
+  <div class="task-actions">
+    <button type="button" class="edit-btn">Edit</button>
+    <button type="button" class="delete-btn">Delete</button>
+  </div>
 `;
 
   // tambahkan taskItem <li> ke dalam taskList <ul>
